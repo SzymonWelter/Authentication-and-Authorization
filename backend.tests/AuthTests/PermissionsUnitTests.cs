95af8d8620e32ccb9f83f6a43b2e553cf6151946
SzymonWelter <szymon.welter98@gmail.com>
2019-03-10 16:03:41 +0100

    init unit tests module

---
+++ backend.tests/AuthTests/PermissionsUnitTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using backend.Auth;
+
+namespace backend.tests.AuthTests
+{
+    public class PermissionsUnitTests
+    {
+        private readonly Permissions permissions;
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
