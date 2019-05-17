5ec29e63d61232c6d7c875dcd3bf833823d0df5d
SzymonWelter <szymon.welter98@gmail.com>
2019-05-18 00:35:31 +0200

    Xor data with divisor in progress

--- Backend/TokenServices/Crc.cs
+++ Backend/TokenServices/Crc.cs
@@ -8,27 +8,60 @@ namespace backend.TokenServices
 {
     public class Crc
     {
-        public byte [] Generate(byte [] token, long polynomial) //returns token+crc
+        public byte [] Message { get; set; }
+        public byte [] Divisor { get; set; }
+        public int CrcLength { get => Divisor.Length * 8 - 1; }
+
+
+        public byte [] Encode() //returns token+crc
         {
-            var newToken = new List<byte>(token);
-            newToken.AddRange(new byte[8]);
-            token = newToken.ToArray();
-            for (int i = 0; i < token.Length; i++)
+            var codeCreator = new List<byte>(Message);            
+            codeCreator.AddRange(new byte[CrcLength]);
+            var code = codeCreator.ToArray();
+            int i = 0;
+            while (IsNextStep())
             {
-                Xor(token, i, BitConverter.GetBytes(polynomial));
+                code = Xor(code,i++);// xor bit by bit
             }
+            return code;
+        }
+        public byte[] Encode(byte[] divisor) //returns token+crc
+        {
+            Divisor = divisor;
+            return Encode();
+        }
+
+        private bool IsNextStep()
+        {
+            
+            
+        }
+
+        private byte [] Xor(byte [] data)
+        {
+            if (data.Length != Divisor.Length) {
+                throw new ArgumentException("The length of both arrays must be the same");
+            }
+            for(int i = 0; i < data.Length; i++)
+            {
+                data[i] = (byte) (data[i] ^ Divisor[i]);
+            }
+            return data;        
+        }
+
+        private byte [] Xor(byte[] data, int offset){
+            //TODO
+            //1) left rotation
+            //2) remove first bit
+            //3) add new bit
+            //4) xor with Divisor
+            //5) return result 
             throw new NotImplementedException();
         }
 
-        private void Xor(byte [] token, int position, byte [] polynomial)
+        public bool Valid(byte[] token, byte [] polynomial)
         {
-            //left shift token position times and xor with polynomial
-            throw new NotImplementedException();
-        }
-
-        public bool Valid(byte[] token, long polynomial)
-        {
-            var result = Generate(token, polynomial);
+            var result = Encode(polynomial);
             return result.All(x => x == 0);
         }
     }
